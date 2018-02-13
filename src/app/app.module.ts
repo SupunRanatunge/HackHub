333e3ed722caf436ee5cd2fdbe813775bf83b7a6
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2018-02-14 00:52:25 +0530

    setting up backend api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import {CarouselComponent} from './components/carousel/carousel.component';
 import {routing} from './app.routing';
 import {HomeComponent} from './pages/home.component';
 import {HackathonComponent} from './pages/hackathon.component';
+import {CreateHackathonComponent} from './pages/createHackathon.component';
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
-  declarations: [ AppComponent, NavbarComponent, CarouselComponent, HomeComponent, HackathonComponent ],
+  declarations: [ AppComponent, NavbarComponent, CarouselComponent, HomeComponent, HackathonComponent, CreateHackathonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
